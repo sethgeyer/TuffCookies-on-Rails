63ca96fff42679569f5134e65a179e3a4763482a
Seth Geyer <seth.geyer@gmail.com>
2013-11-18 13:29:38 -0700

    initial setup for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TcOnRails::Application.config.secret_token = 'f7933b6a41b38087880984971b077bb01f9a684c26b95a3a2d87ea29caca38421fc9e8358dba56d5c5ca8ad2fd86d495d476afef306d71a0303e6f0cf7accc6c'
